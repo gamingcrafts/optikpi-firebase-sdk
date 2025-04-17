9e89ace7cbc4f467350f652839c475ea38111d23
imchandrusankar16 <chandru.sankar@digient.in>
2025-04-17 15:56:24 +0530

    pass workspaceId/customerId in login and logout method

--- optikpi-push-sdk.js
+++ optikpi-push-sdk.js
@@ -1,135 +1,151 @@
-
 class PushSDK {
-    subscription = null;
+  subscription = null;
 
-    constructor(optiKPIPushKey, optiKPICustomerId, originURL = "https://push.optikpi.com", serviceWorkerPath = '/optikpi-service-worker.js') {
-        this.optiKPIPushKey = optiKPIPushKey;
-        this.optiKPICustomerId = optiKPICustomerId;
-        this.originURL = originURL;
-        this.serviceWorkerPath = serviceWorkerPath;
+  constructor(
+    optiKPIPushKey,
+    originURL = "https://push.optikpi.com",
+    serviceWorkerPath = "/optikpi-service-worker.js"
+  ) {
+    this.optiKPIPushKey = optiKPIPushKey;
+    this.originURL = originURL;
+    this.serviceWorkerPath = serviceWorkerPath;
+  }
+
+  async requestNotificationPermission() {
+    try {
+      const permission = await Notification.requestPermission();
+      if (permission === "granted") {
+        console.log("Notification permission granted.");
+        return true;
+      } else {
+        console.warn("Notification permission denied.");
+        return false;
+      }
+    } catch (error) {
+      console.error("Failed to request notification permission:", error);
+      return false;
     }
+  }
 
-    async requestNotificationPermission() {
-        try {
-            const permission = await Notification.requestPermission();
-            if (permission === 'granted') {
-              console.log('Notification permission granted.');
-              return true;
-            } else {
-              console.warn('Notification permission denied.');
-              return false;
-            }
-        } catch (error) {
-            console.error('Failed to request notification permission:', error);
-            return false;
-        }
+  async registerServiceWorker() {
+    if ("serviceWorker" in navigator) {
+      try {
+        const registration = await navigator.serviceWorker.register(
+          this.serviceWorkerPath
+        );
+        return registration;
+      } catch (error) {
+        console.error("Service Worker registration failed:", error);
+        throw error;
+      }
+    } else {
+      throw new Error("Service Workers are not supported in this browser.");
     }
+  }
 
-    async registerServiceWorker() {
-        if ('serviceWorker' in navigator) {
-            try {
-                const registration = await navigator.serviceWorker.register(this.serviceWorkerPath);
-                return registration;
-            } catch (error) {
-                console.error('Service Worker registration failed:', error);
-                throw error;
-            }
-        } else {
-            throw new Error('Service Workers are not supported in this browser.');
-        }
+  async subscribeUser() {
+    try {
+      const registration = await this.registerServiceWorker();
+      const subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: this.urlBase64ToUint8Array(this.optiKPIPushKey),
+      });
+      console.log("User is subscribed:");
+      return subscription;
+    } catch (error) {
+      console.error("Failed to subscribe the user:", error);
+      throw error;
     }
+  }
 
-    async subscribeUser() {
-        try {
-            const registration = await this.registerServiceWorker();
-            const subscription = await registration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: this.urlBase64ToUint8Array(this.optiKPIPushKey),
-            });
-            console.log('User is subscribed:');
-            return subscription;
-        } catch (error) {
-            console.error('Failed to subscribe the user:', error);
-            throw error;
-        }
+  async sendSubscriptionToServer(subscription) {
+    const response = await fetch(`${this.originURL}/storeSubscription`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(subscription),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to send subscription to server.");
     }
+    console.log("Subscription sent to server.");
+    return await response.json();
+  }
 
-    async sendSubscriptionToServer(subscription) {
-        const response = await fetch(`${this.originURL}/storeSubscription`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(subscription),
-        });
-        if (!response.ok) {
-            throw new Error('Failed to send subscription to server.');
-        }
-        console.log('Subscription sent to server.');
-        return await response.json();
+  urlBase64ToUint8Array(base64String) {
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+      .replace(/-/g, "+")
+      .replace(/_/g, "/");
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
     }
+    return outputArray;
+  }
 
-    urlBase64ToUint8Array(base64String) {
-        const padding = '='.repeat((4 - base64String.length % 4) % 4);
-        const base64 = (base64String + padding)
-            .replace(/-/g, '+')
-            .replace(/_/g, '/');
-
-        const rawData = window.atob(base64);
-        const outputArray = new Uint8Array(rawData.length);
-        for (let i = 0; i < rawData.length; ++i) {
-            outputArray[i] = rawData.charCodeAt(i);
-        }
-        return outputArray;
+  async init() {
+    try {
+      const hasRequestNotificationPermission =
+        await this.requestNotificationPermission();
+      if (hasRequestNotificationPermission) {
+        this.subscription = await this.subscribeUser();
+        await this.sendSubscriptionToServer(this.subscription);
+      }
+    } catch (error) {
+      console.error("Error during initialization:", error);
     }
+  }
 
-    async init() {
-        try {
-            const hasRequestNotificationPermission = await this.requestNotificationPermission();
-            if(hasRequestNotificationPermission) {
-                this.subscription = await this.subscribeUser();
-                await this.sendSubscriptionToServer(this.subscription);
-            }
-        } catch (error) {
-            console.error('Error during initialization:', error);
-        }
+  async registerPushToken(userId, customerId) {
+    const url = `${this.originURL}/associatePlayer`;
+    try {
+      const registerResponse = await this.handleAPICallout(
+        url,
+        userId,
+        customerId
+      );
+      console.log(" successfully associatePlayer");
+      return registerResponse;
+    } catch (err) {
+      throw new Error("Failed to associatePlayer");
     }
+  }
 
-    async registerPushToken(userId) {
-        const url = `${this.originURL}/associatePlayer`;
-        try{
-            const registerResponse = await this.handleAPICallout(url, userId);
-            return registerResponse;
-        } catch(err){
-            throw new Error('Failed to associatePlayer');
-        }
-    }
+  async unRegisterPushToken(userId, customerId) {
+    const url = `${this.originURL}/disassociatePlayer`;
+    try {
+      const unregisterResponse = await this.handleAPICallout(
+        url,
+        userId,
+        customerId
+      );
+      console.log(" successfully disassociatePlayer");
 
-    async unRegisterPushToken(userId) {
-        const url = `${this.originURL}/disassociatePlayer`;
-        try{
-            const unregisterResponse = await this.handleAPICallout(url, userId);
-            return unregisterResponse;
-        } catch(err){
-            throw new Error('Failed to disassociatePlayer');
-        }
+      return unregisterResponse;
+    } catch (err) {
+      throw new Error("Failed to disassociatePlayer");
     }
+  }
 
-    async handleAPICallout(url, userId) {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                userId: userId,
-                subscription: this.subscription,
-                workspaceId: this.optiKPICustomerId,   
-            })
-        });
-        if (!response.ok) {
-            throw new Error('Failed to register push token');
-        }
-        return await response.json();
+  async handleAPICallout(url, userId, customerId) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: userId,
+        subscription: this.subscription,
+        workspaceId: customerId,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to register push token");
     }
+    return await response.json();
+  }
 }
--- optikpi-service-worker.js
+++ optikpi-service-worker.js
@@ -1,23 +1,21 @@
-self.addEventListener('push', function(event) {
-    const notificationData = event.data.json();
-    const options = {
-      badge: notificationData?.badge,
-      body: notificationData?.body,
-      data: notificationData?.data,
-      icon: notificationData?.icon,
-      image: notificationData?.image,
-      title: notificationData?.title,
-      lang: notificationData?.lang,
-    };
-    event.waitUntil(
-      self.registration.showNotification('Push Notification', options)
-    );
-  });
-  
-  // self.addEventListener('notificationclick', function(event) {
-  //   event.notification.close();
-  //   event.waitUntil(
-  //     clients.openWindow('')
-  //   );
-  // });
-  
+self.addEventListener("push", function (event) {
+  const notificationData = event.data.json();
+  const title = notificationData?.title || "Push Notification";
+  const options = {
+    badge: notificationData?.badge,
+    body: notificationData?.body,
+    data: notificationData?.data,
+    icon: notificationData?.icon,
+    image: notificationData?.image,
+    title: notificationData?.title,
+    lang: notificationData?.lang,
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+// self.addEventListener('notificationclick', function(event) {
+//   event.notification.close();
+//   event.waitUntil(
+//     clients.openWindow('')
+//   );
+// });
